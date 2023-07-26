071204ae35d8487f4a8f182c04573b4c8943683d
pradeepsb55@gmail.com <Pradeep@55555>
2023-07-26 17:05:48 +0530

    adapter update

--- src/main/java/com/imageupload/config/SecurityConfiguration.java
+++ src/main/java/com/imageupload/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,7 +19,7 @@ import com.imageupload.service.UserService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration implements WebMvcConfigurer  {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter    {
 
   @Autowired
   private UserService userService;
